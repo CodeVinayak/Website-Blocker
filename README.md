c15dc258a553f92b319a22fcb1f1a77da92e368d
Vinayak Singh <vinayaksingh10142@gmail.com>
2023-02-21 01:02:10 +0530

    initial push
    Introducing our new Website Blocker extension! Say goodbye to distracting websites and hello to increased productivity. Download now to start blocking sites and taking control of your browsing habits.

---
+++ README.md
@@ -0,0 +1,9 @@
+Extension to help you stay focused on your work.
+
+[![Twitter Follow](https://img.shields.io/twitter/follow/CodeVinayak)](https://twitter.com/CodeVinayak)
+
+<br/>
+
+<h2 align="center">
+  Made by Vinayak Singh.
+</h2>
---
+++ blocker.js
@@ -0,0 +1,18 @@
+const blockedSites = ['www.youtube.com', 'www.facebook.com', 'www.instagram.com'];
+const blockedImages = ['https://media.tenor.com/q2eL6vNVKf4AAAAC/bhai-kya-kar-raha-hai-tu-ashneer-grover.gif', 'https://media.tenor.com/MtsRCM6XD1wAAAAC/carryminati-ajey-nagar.gif', 'https://media.tenor.com/n7lK1BhIPUgAAAAC/padhai-likhai-karo-munna-tripathi.gif'];
+const blockedMessageHTML = `<div>
+<p>Access to this site is blocked on this device.</p>
+<br>
+<img src="${blockedImages[Math.floor(Math.random() * blockedImages.length)]}" alt="Blocked GIF">
+</div>`;
+
+if (blockedSites.includes(window.location.hostname)) {
+  const styleEl = document.createElement('style');
+  styleEl.innerHTML = `body{margin:0;padding:0;height:100vh;display:flex;justify-content:center;align-items:center;background-color:#fff;color:#000;font-family:Arial,sans-serif;font-size:24px;}`;
+  document.head.appendChild(styleEl);
+
+  const blockedDiv = document.createElement('div');
+  blockedDiv.innerHTML = blockedMessageHTML;
+  document.body.innerHTML = '';
+  document.body.appendChild(blockedDiv);
+}
